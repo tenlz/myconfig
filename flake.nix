ffe05e7aa2ebceb029c1ba72e0498a6795946520
tenlz <shmily@gmail.com>
2023-11-30 12:25:02 +0800

    first

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "shmily's NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      #     nixos-rebuild switch --flake .#nixos
+      "nixos" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./configuration.nix
+
+         home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # 这里的 ryan 也得替换成你的用户名
+            # 这里的 import 函数在前面 Nix 语法中介绍过了，不再赘述
+            home-manager.users.shmily = import ./home.nix;
+           }
+
+        ];
+      };
+    };
+  };
+}
---
+++ gnome.nix
@@ -0,0 +1,50 @@
+{ pkgs
+, ...
+}:
+
+{
+  imports = [ ];
+  config = {
+    services.xserver.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.excludePackages = [ pkgs.xterm ];
+    services.xserver.desktopManager.xterm.enable = false;
+    services.gnome = {
+      core-utilities.enable = false;
+      gnome-online-accounts.enable = false;
+      gnome-browser-connector.enable = true;
+      tracker-miners.enable = false;
+      tracker.enable = false;
+    };
+    environment.gnome.excludePackages = (with pkgs; [
+      gnome-tour
+      orca
+      #]) ++ (with pkgs.gnome; [
+    ]);
+    programs.dconf.enable = true;
+    # fix nautilus extensions deu to `core-utilities.enable = false`
+    # workarounds.gnome-fix.enable = true;
+    environment.systemPackages = (with pkgs; [
+      remmina
+      ffmpegthumbnailer
+      amberol # music
+      loupe # image
+      gnome-text-editor
+      gnome-console
+      epiphany
+    ]) ++ (with pkgs.gnome; [
+      nautilus # file
+      file-roller # archive
+      evince # pdf
+      gnome-tweaks
+      gnome-calendar
+      dconf-editor
+      seahorse
+      gnome-disk-utility
+      # loupe # https://github.com/NixOS/nixpkgs/pull/202692
+      # gnome-sound-recorder
+      # gnome-power-manager
+    ]);
+  };
+}
