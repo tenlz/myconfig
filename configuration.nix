c607eb893efc5589e216b64235c7f6b7592f6cba
tenlz <shmily@gmail.com>
2023-12-01 21:42:29 +0800

    8

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,7 @@
       ./gnome.nix
       ./fonts.nix
       ./ibus.nix
-      
+   
     ];
 
   # Bootloader.
--- services.nix
+++ services.nix
@@ -6,10 +6,10 @@
     transmission = {
       enable = true;
       package = pkgs.transmission_4;
-      user = "zzzsy";
+      user = "shmily";
       group = "users";
       downloadDirPermissions = "755";
-      home = "/home/zzzsy";
+      home = "/home/shmily";
       settings = {
         download-dir = "${config.services.transmission.home}/Downloads/Transmission";
         incomplete-dir-enabled = false;
