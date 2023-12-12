0985ca8631192a4145d6c6cb946182ca7ce1654b
tenlz <shmily@gmail.com>
2023-12-12 10:54:55 +0800

    add neovim

--- neovim.nix
+++ neovim.nix
@@ -3,12 +3,9 @@
 {
     programs.neovim = {
     enable = true;
-    withPython3 = true;
-    withNodeJs = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    vimdiffAlias = true;
   };
 
 }
